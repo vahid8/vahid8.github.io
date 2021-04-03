2f4143ae3921d666f7f7d02b1f5cbf8ebe53f828
vahid <aghajanivahid1@gmail.com>
2021-04-03 12:54:48 +0200

    animation_added

---
+++ main.js
@@ -0,0 +1,6 @@
+gsap.to(".intro_image",{duration: 3,height:"100%",top:"0",opacity:"100%",ease: "slow"});
+// gsap.to(".intro_image",{delay:2,duration: 1,width:"50%",left:"50%"});
+gsap.to(".intro_label",{duration: 3,opacity:"100%",bottom:"40%",ease: "slow"});
+// gsap.to(".container",{duration:2,backgroundColor:"black"})
+// gsap.to(".intro_image",{top:"0"});
+// gsap.to(".leftside-col",{duration: 3,bottom:"30%",color:"white",ease: "slow"})
